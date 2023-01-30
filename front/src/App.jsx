d9de10f81b1475c4480219612257c1d423416ce3
vgulbickaja <106967101+vgulbickaja@users.noreply.github.com>
2023-01-30 11:48:53 +0200

    added logo, style to navbar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.scss";
 import Navbar from "./Components/Navbar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={}></Route>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- front/src/Components/Navbar.jsx
+++ front/src/Components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import horse from "../Assets/horse.jpg";
+import { Link } from "react-router-dom";
+import "../Styles/Navbar.scss";
 
 function Navbar() {
   return (
@@ -6,7 +9,12 @@ function Navbar() {
       <div className="leftSide">
         <img src={horse} />
       </div>
-      <div className="rightSide"></div>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/main"> Main </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+      </div>
     </div>
   );
 }
