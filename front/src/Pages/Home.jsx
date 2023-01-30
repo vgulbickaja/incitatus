82983c796708592eae53ec09d686405377d68417
vgulbickaja <106967101+vgulbickaja@users.noreply.github.com>
2023-01-30 12:09:10 +0200

    home page added

--- front/src/Pages/Home.jsx
+++ front/src/Pages/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import banner from "../Assets/banner.jpg";
+import "../Styles/Home.scss";
 
 function Home() {
-  <div>Home</div>;
+  return (
+    <div className="home" style={{ backgroundImage: `url(${banner})` }}>
+      <div className="headerContainer">
+        <h1>Incitatus</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit fugiat
+          est, a aliquid labore mollitia at ex tempora rem. Exercitationem
+          blanditiis ea eius, modi minus deserunt delectus asperiores facere
+          illo.
+        </p>
+        <Link to="/menu">
+          <button>Purchase</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
